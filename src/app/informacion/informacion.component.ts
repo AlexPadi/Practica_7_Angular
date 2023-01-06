ef63f995db5eaf85b1d8352b0103dfb5c87105e8
AlexPadi <119027715+AlexPadi@users.noreply.github.com>
2023-01-06 15:48:14 -0500

    Add files via upload

--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-informacion',
+  selector: 'informacion',
   templateUrl: './informacion.component.html',
   styleUrls: ['./informacion.component.css']
 })
 export class InformacionComponent {
-
+  constructor(){}
+  ngOnInit(): void{}
+  deducibles=[{name:'Vivienda',img:"/assets/casa.jpeg"},{name:'Salud',img:"/assets/educacion.jpeg"},{name:'Educacion',img:"/assets/salud.jpeg"}];
+  informacion(){
+    alert('Esta es información adicional');
+  }
+  borrarDeducible(deducible: any){
+    for (let i = 0; i < this.deducibles.length; i++) {
+      if (deducible==this.deducibles[i]) {
+        this.deducibles.splice(i,1);
+      }
+      
+    }
+  }
 }
